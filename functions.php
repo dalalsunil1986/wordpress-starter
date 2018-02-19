5a483ca55894d23593e76279c6bbbdd4270237de
Calvin Koepke <hello@calvinkoepke.com>
2018-02-19 16:09:53 -0700

    prefer instantiation of the Uno workflow on the child theme level.

--- functions.php
+++ functions.php
@@ -4,8 +4,9 @@ namespace Uno;
 
 /**
  * The main functions.php file for the Uno Genesis child theme. This file is
- * not intended to have anything other than the child theme setup file. All
- * the other theme files should be added to the autoloaded /src/ folder.
+ * not intended to have anything other than the instantiation of the Uno class.
+ * All other setup files for your child theme should be added to the /src/ folder,
+ * which will autoload everything in it.
  *
  * @package    Uno Free
  * @author     Calvin Koepke <hello@calvinkoepke.com>
@@ -17,6 +18,16 @@ namespace Uno;
  */
 
 /**
- * Load the Uno child theme init file.
+ * Import the main Uno class dependency.
+ *
+ * @since 0.0.1
  */
-require_once( get_stylesheet_directory() . '/vendor/uno/uno-package/init.php' );
+require_once( get_stylesheet_directory() . '/vendor/uno/uno-package/Uno.class.php' );
+
+/**
+ * Instantiate the class to enable the development workflow. You can also call
+ * Uno::init() statically to achieve the same result, without instantiation.
+ *
+ * @since 0.0.1
+ */
+$uno = new Uno();
